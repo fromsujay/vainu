286b530afc24be231a32a2b9db6cb4fdcecd70a4
Sujay PATIL <fromsujay@gmail.com>
2018-05-12 17:32:20 +0200

    Addition of comments

--- darkskymap/models.py
+++ darkskymap/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# Create your models here. Each field below represents column in database table.
 class Locationdata(models.Model):
     locationid = models.CharField(max_length = 70, unique=True)
     latitude = models.FloatField()
--- darkskymap/views.py
+++ darkskymap/views.py
@@ -10,10 +10,13 @@ from .models import Locationdata
 from .forms import LocationdataForm
 
 
-# This function and associated data is passed onto urls pattern in urls.py of application
+# These functions and associated data is passed onto urls pattern in urls.py of application
+# This function renders landing page view
 def accueil(request):
     return render(request, 'darkskymap/accueil.html', {})
     
+# This function renders map/découvrir view page. Datadump extracts data from database
+# and serves it as dictionary to map/découvrir html page
 def decouvrir(request):
     datadump_obj = Locationdata.objects.all().values()
     datadump_lst = [row for row in datadump_obj]
@@ -21,6 +24,8 @@ def decouvrir(request):
     datadump['datadump'] = datadump_lst
     return render(request, 'darkskymap/decouvrir.html', datadump)
 
+# This function renders dataentry/partager page view. If success, user is redirected to
+# map/découvrir page, if error a message is served otherwise a virgin form is presented.
 def partager(request):
     if request.method == 'POST':
         form = LocationdataForm(request.POST)
